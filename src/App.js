0379c6d05e733013c71537031a15ab0d6b94db97
yeona23 <136429602+yeona23@users.noreply.github.com>
2023-10-19 15:23:00 +0900

    count기능 구현
    리액트 공부
    useEffect
    useRef

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
+import { useRef, useEffect, useState } from "react";
 import "./App.css";
-import Body from "./components/Body";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-
-// const ChildComp = () => <div>Child components</div>;
+import Controller from "./component/Controller";
+import Viewer from "./component/Viewer";
+import Even from "./component/Even";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const handleSetCount = (value) => {
+    setCount(count + value);
+  };
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  const didMountRef = useRef(false);
+
   return (
     <div className="App">
-      <Header />
-      <Body></Body>
-      <Footer />
+      <h1>Simple Counter</h1>
+      <section>
+        <input value={text} onChange={handleChangeText} />
+      </section>
+      <section>
+        <Viewer count={count} />
+        {count % 2 === 0 && <Even />}
+      </section>
+      <section>
+        <Controller handleSetCount={handleSetCount} />
+      </section>
     </div>
   );
 }
---
+++ src/component/Viewer.js
@@ -0,0 +1,10 @@
+const Viewer = ({ count }) => {
+  return (
+    <div>
+      <div>present count: </div>
+      <h1>{count}</h1>
+    </div>
+  );
+};
+
+export default Viewer;
